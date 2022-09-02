aeae9ec234d3d3620849e251d001b41624c8e2af
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-09-02 11:59:15 -0300

    Seccion 03: 21. Múltiples exportaciones y exportaciones por defecto

--- 02-intro-javascript/src/bases/08-imp-exp.js
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -1,4 +1,6 @@
-import { heroes } from './data/heroes';
+import heroes, { owners } from '../data/heroes';
+
+console.log(owners);
 
 export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
 
